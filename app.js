8dd28ae14c01b1ac689d2b413d735bd697304c3f
nano <nanoshdee@gmail.com>
2022-01-21 12:04:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -106,92 +106,92 @@ const htmlTagRemover = (content) => {
 
       if(item !== ".DS_Store"){
 
-        const readStream=require('fs').createReadStream(`${inputPath + item}.csv`);
-        const writeStream=require("fs").createWriteStream(`${outputPath}/${item}.json`);
+        // const readStream=require('fs').createReadStream(`${inputPath + item}.csv`);
+        // const writeStream=require("fs").createWriteStream(`${outputPath}/${item}.json`);
 
-        readStream.pipe(CSVToJSON({
-          noheader: true,
-          headers: headers,
-        })).pipe(writeStream);
-        
-        // const resp = await CSVToJSON({
+        // readStream.pipe(CSVToJSON({
         //   noheader: true,
         //   headers: headers,
-        //   output: "json"
-        // }).fromFile(`${inputPath + item}.csv`)
+        // })).pipe(writeStream);
+        
+        const resp = await CSVToJSON({
+          noheader: true,
+          headers: headers,
+          output: "json"
+        }).fromFile(`${inputPath + item}.csv`)
 
-        // fs.writeFileSync(
-        //   `${outputPath}/${item}.json`,
-        //   JSON.stringify(resp),
-        //   "utf-8",
-        //   (err) => {
-        //     if (err) {
-        //       throw err;
-        //     }
-        //   }
-        // );
+        fs.writeFileSync(
+          `${outputPath}/${item}.json`,
+          JSON.stringify(resp),
+          "utf-8",
+          (err) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
     
-        // await htmlTagRemover(item);
+        await htmlTagRemover(item);
   
-        // const str = fs.readFileSync(`${outputPath}/${item}.json`, "utf8");
-        // const obj = JSON.parse(str);
-        // let newObj = {};
-        // let newArr = [];
-        // let id;
-        // for (let i = 0; i < obj.length; i++) {
-        //   let date = new Date(obj[i].createdAt);
-        //   let createdAt = date.toISOString();
+        const str = fs.readFileSync(`${outputPath}/${item}.json`, "utf8");
+        const obj = JSON.parse(str);
+        let newObj = {};
+        let newArr = [];
+        let id;
+        for (let i = 0; i < obj.length; i++) {
+          let date = new Date(obj[i].createdAt);
+          let createdAt = date.toISOString();
 
 
-        //   if (id !== obj[i].oldCaakId) {
-        //     newObj = {
-        //       oldCaakId: obj[i].oldCaakId,
-        //       title: obj[i].title,
-        //       description: `${obj[i].brief}`,
-        //       f_text: obj[i].footer,
-        //       updatedAt: createdAt,
-        //       createdAt: createdAt,
-        //       items: [],
-        //     };
+          if (id !== obj[i].oldCaakId) {
+            newObj = {
+              oldCaakId: obj[i].oldCaakId,
+              title: obj[i].title,
+              description: `${obj[i].brief}`,
+              f_text: obj[i].footer,
+              updatedAt: createdAt,
+              createdAt: createdAt,
+              items: [],
+            };
   
-        //     newArr.push(newObj);
-        //   }
+            newArr.push(newObj);
+          }
 
-        //   if(i === 0){
-        //     newObj.items.push({
-        //       title: `${obj[i].block_description}`,
-        //       description: `${(obj[i].header ? obj[i].header + " &#13; ": "")}${obj[i].description ? obj[i].description : ""}`,
-        //       block_img: obj[i].cover_image,
-        //       block_type: "image",
-        //       video_provider: obj[i].video_provider,
-        //       video_id: obj[i].video_id
-        //     });
-        //   }
+          if(i === 0){
+            newObj.items.push({
+              title: `${obj[i].block_description}`,
+              description: `${(obj[i].header ? obj[i].header + " &#13; ": "")}${obj[i].description ? obj[i].description : ""}`,
+              block_img: obj[i].cover_image,
+              block_type: "image",
+              video_provider: obj[i].video_provider,
+              video_id: obj[i].video_id
+            });
+          }
 
-        //   id = obj[i].oldCaakId;
+          id = obj[i].oldCaakId;
   
-        //   newObj.items.push({
-        //     title: `${obj[i].block_description}`,
-        //     description: `${obj[i].block_title}`,
-        //     block_img: obj[i].block_img,
-        //     block_type: obj[i].block_type,
-        //     video_provider: obj[i].video_provider,
-        //     video_id: obj[i].video_id
-        //   });
-        // }
+          newObj.items.push({
+            title: `${obj[i].block_description}`,
+            description: `${obj[i].block_title}`,
+            block_img: obj[i].block_img,
+            block_type: obj[i].block_type,
+            video_provider: obj[i].video_provider,
+            video_id: obj[i].video_id
+          });
+        }
   
-        // fs.writeFileSync(
-        //   `${outputPath}/${item}.json`,
-        //   JSON.stringify(newArr),
-        //   "utf-8",
-        //   (err) => {
-        //     if (err) {
-        //       throw err;
-        //     }
-        //   }
-        // );
+        fs.writeFileSync(
+          `${outputPath}/${item}.json`,
+          JSON.stringify(newArr),
+          "utf-8",
+          (err) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
 
-        // console.log(`\r${counter}:${item}.json`);
+        console.log(`\r${counter}:${item}.json`);
       }
     }
 
