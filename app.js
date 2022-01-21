d208a15e515fcdc436f4dafb04682f228566e684
nano <nanoshdee@gmail.com>
2022-01-21 11:54:47 +0800

    a

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const figlet = require("figlet");
 
 const Spinner = require("cli-spinner").Spinner;
 const SPINNER_STRING = "⠋⠙⠹⠸⠼⠴⠦⠧⠏";
-const spinner = new Spinner("converting..");
+// const SPINNER_STRING = "|/-|/-";
+const spinner = new Spinner("converting...");
 spinner.setSpinnerString(SPINNER_STRING);
 
 const headers = [
@@ -20,6 +21,13 @@ const headers = [
   "block_title",
   "block_description",
   "block_img",
+  "header",
+  "footer",
+  "block_type",
+  "video_provider",
+  "video_id",
+  "cover_image",
+  "brief"
 ];
 
 const inputPath = "./input/";
@@ -38,13 +46,32 @@ const getFilePaths = (input) => {
 
 const htmlTagRemover = (content) => {
   try {
+
     const data = fs.readFileSync(`${outputPath}/${content}.json`, "utf8");
-    const dataStr = JSON.stringify(data);
-    const res = dataStr.replace(/<[^<>]+>/gi, "");
+    const jsonData = JSON.parse(data)
+
+    for(let i=0; i < jsonData.length; i ++){
+
+        const curKeys = Object.keys(jsonData[i])
+
+        if(i === 0){
+          console.log(curKeys)
+        }
+
+        for(let keysIndex = 0; keysIndex < curKeys.length; keysIndex++){
+            const curKey = curKeys[keysIndex]
+            if(curKey !== "footer"){
+              jsonData[i][curKey] = jsonData[i][curKey].replace(/<[^<>]+>/gi, "")
+            }
+        }
+    }
+
+    const dataStr = JSON.stringify(jsonData);
+    // const res = dataStr.replace(/<[^<>]+>/gi, "");
 
     fs.writeFileSync(
       `${outputPath}/${content}.json`,
-      JSON.parse(res),
+      dataStr,
       "utf-8",
       (err) => {
         if (err) {
@@ -72,76 +99,107 @@ const htmlTagRemover = (content) => {
 
   try {
     const paths = await getFilePaths(inputPath);
+
     spinner.start();
+    for(let pIndex=0; pIndex < paths.length; pIndex++) {
+      const item = paths[pIndex]
 
-    paths.map(async (item) => {
-      const resp = await CSVToJSON({
-        ignoreEmpty: true,
-        noheader: true,
-        headers: headers,
-      }).fromFile(`${inputPath + item}.csv`);
+      if(item !== ".DS_Store"){
 
-      fs.writeFileSync(
-        `${outputPath}/${item}.json`,
-        JSON.stringify(resp, null, 4),
-        "utf-8",
-        (err) => {
-          if (err) {
-            throw err;
-          }
-        }
-      );
-      await htmlTagRemover(item);
+        const readStream=require('fs').createReadStream(`${inputPath + item}.csv`);
+        const writeStream=require("fs").createWriteStream(`${outputPath}/${item}.json`);
 
-      const str = fs.readFileSync(`${outputPath}/${item}.json`, "utf8");
-      const obj = JSON.parse(str);
-      let newObj = {};
-      let newArr = [];
-      let id;
-      for (let i = 0; i < obj.length; i++) {
-        let date = new Date(obj[i].createdAt);
-        let createdAt = date.toISOString();
-        if (id !== obj[i].oldCaakId) {
-          newObj = {
-            oldCaakId: obj[i].oldCaakId,
-            title: obj[i].title,
-            description: obj[i].description,
-            updatedAt: createdAt,
-            createdAt: createdAt,
-            items: [],
-          };
+        readStream.pipe(CSVToJSON({
+          noheader: true,
+          headers: headers,
+        })).pipe(writeStream);
+        
+        // const resp = await CSVToJSON({
+        //   noheader: true,
+        //   headers: headers,
+        //   output: "json"
+        // }).fromFile(`${inputPath + item}.csv`)
 
-          newArr.push(newObj);
-        }
-        id = obj[i].oldCaakId;
+        // fs.writeFileSync(
+        //   `${outputPath}/${item}.json`,
+        //   JSON.stringify(resp),
+        //   "utf-8",
+        //   (err) => {
+        //     if (err) {
+        //       throw err;
+        //     }
+        //   }
+        // );
+    
+        // await htmlTagRemover(item);
+  
+        // const str = fs.readFileSync(`${outputPath}/${item}.json`, "utf8");
+        // const obj = JSON.parse(str);
+        // let newObj = {};
+        // let newArr = [];
+        // let id;
+        // for (let i = 0; i < obj.length; i++) {
+        //   let date = new Date(obj[i].createdAt);
+        //   let createdAt = date.toISOString();
 
-        newObj.items.push({
-          title: obj[i].block_description,
-          block_img: obj[i].block_img,
-        });
+
+        //   if (id !== obj[i].oldCaakId) {
+        //     newObj = {
+        //       oldCaakId: obj[i].oldCaakId,
+        //       title: obj[i].title,
+        //       description: `${obj[i].brief}`,
+        //       f_text: obj[i].footer,
+        //       updatedAt: createdAt,
+        //       createdAt: createdAt,
+        //       items: [],
+        //     };
+  
+        //     newArr.push(newObj);
+        //   }
+
+        //   if(i === 0){
+        //     newObj.items.push({
+        //       title: `${obj[i].block_description}`,
+        //       description: `${(obj[i].header ? obj[i].header + " &#13; ": "")}${obj[i].description ? obj[i].description : ""}`,
+        //       block_img: obj[i].cover_image,
+        //       block_type: "image",
+        //       video_provider: obj[i].video_provider,
+        //       video_id: obj[i].video_id
+        //     });
+        //   }
+
+        //   id = obj[i].oldCaakId;
+  
+        //   newObj.items.push({
+        //     title: `${obj[i].block_description}`,
+        //     description: `${obj[i].block_title}`,
+        //     block_img: obj[i].block_img,
+        //     block_type: obj[i].block_type,
+        //     video_provider: obj[i].video_provider,
+        //     video_id: obj[i].video_id
+        //   });
+        // }
+  
+        // fs.writeFileSync(
+        //   `${outputPath}/${item}.json`,
+        //   JSON.stringify(newArr),
+        //   "utf-8",
+        //   (err) => {
+        //     if (err) {
+        //       throw err;
+        //     }
+        //   }
+        // );
+
+        // console.log(`\r${counter}:${item}.json`);
       }
+    }
 
-      fs.writeFileSync(
-        `${outputPath}/${item}.json`,
-        JSON.stringify(newArr),
-        "utf-8",
-        (err) => {
-          if (err) {
-            throw err;
-          }
-        }
-      );
-
-      counter = counter + 1;
-      console.log(`\r${counter}:${item}.json`);
-
-      if (counter === paths.length) {
-        console.log("------Convert Finished------");
-        spinner.stop(true);
-      }
-    });
-  } catch (error) {
+    console.log("------Convert Finished------");
     spinner.stop(true);
+
+  } catch (error) {
     console.log(error);
+    spinner.stop(true);
   }
 })();
